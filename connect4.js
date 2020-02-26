d5d484b9eabe0de0ac52e7485877d3e8c2aa321a
Trenton Marks <trentonmarks@Trentons-MacBook-Air.local>
2020-02-26 14:39:12 -0500

    Added functionality to check if a player has won after each move

--- connect4.js
+++ connect4.js
@@ -4,12 +4,17 @@ class Connect4 {
         this.cols = 7;
         this.selector = selector;
         this.player = 'red';
+        this.gameIsOver = false;
+        this.onPlayerMove = function(){};
         this.createGrid();
         this.setupEventListeners();
     }
 
     createGrid() {
         const $board = $(this.selector);
+        $board.empty();
+        this.gameIsOver = false;
+        this.player = 'red';
         for (let row = 0; row < this.rows; row++) {
             const $row = $('<div>')
                 .addClass('row');
@@ -38,7 +43,9 @@ class Connect4 {
         }
 
         $board.on('mouseenter', '.col.empty', function() {
+            if (that.gameIsOver) return;
             const col = $(this).data('col');
+            const row = $(this).data('row');
             const $lastEmptyCell = findLastEmptyCell(col);
             $lastEmptyCell.addClass(`next-${that.player}`);
         });
@@ -48,13 +55,92 @@ class Connect4 {
         });
 
         $board.on('click', '.col.empty', function() {
+            if (that.gameIsOver) return;
             const col = $(this).data('col');
             const $lastEmptyCell = findLastEmptyCell(col);
             $lastEmptyCell.removeClass(`empty next-${that.player}`)
-            console.log($lastEmptyCell);
             $lastEmptyCell.addClass(that.player);
+            $lastEmptyCell.data('player', that.player);
+
+            const winner = that.checkForWinner(
+                $lastEmptyCell.data('row'),
+                $lastEmptyCell.data('col')
+            );
+            if (winner) {
+                that.gameIsOver = true;
+                alert(`Game Over! Player ${that.player} has won!`);
+                $('.col.empty').removeClass('empty');
+                return;
+            }
             that.player = (that.player === 'red') ? 'black' : 'red';
+            that.onPlayerMove();
             $(this).trigger('mouseenter');
         });
+    }
+
+    checkForWinner(row, col) {
+        const that = this;
+
+        function $getCell(i, j) {
+            return $(`.col[data-row=${i}][data-col=${j}]`);
+        }
+
+        function checkDirection(direction) {
+            let total = 0;
+            let i = row + direction.i;
+            let j = col + direction.j;
+            let $next = $getCell(i, j);
+            while (
+                i >= 0 &&
+                i < that.rows &&
+                j >= 0 &&
+                j < that.cols &&
+                $next.data('player')  === that.player
+            ) {
+                total++;
+                i += direction.i;
+                j += direction.j;
+                $next = $getCell(i, j);
+            }
+            return total;
+        }
+
+        function checkWin(directionA, directionB) {
+            const total = 1 +
+                checkDirection(directionA) +
+                checkDirection(directionB);
+            if (total >= 4) {
+                return that.player;
+            } else {
+                return null;
+            }
+
+        }
+
+        function checkVerticals() {
+            return checkWin({i: -1, j: 0}, {i: 1, j: 0});
+        }
+
+        function checkHorizontals() {
+            return checkWin({i: 0, j: -1}, {i: 0, j: 1});
+        }
+
+        function checkDiagonalsBLtoTR() {
+            return checkWin({i: 1, j: -1}, {i: 1, j: 1});
+        }
+
+        function checkDiagonalsBRtoTL() {
+            return checkWin({i: 1, j: 1}, {i: -1, j: -1});
+        }
+
+        return checkVerticals() ||
+               checkHorizontals() ||
+               checkDiagonalsBLtoTR() ||
+               checkDiagonalsBRtoTL();
+    }
+
+    restart() {
+        this.createGrid();
+        this.onPlayerMove();
     }
 }
--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(() => {
     const connect4 = new Connect4('#connect4');
+
+    connect4.onPlayerMove = function() {
+        $('#player').text(connect4.player);
+    }
+
+    $('#restart').on('click', function(){
+        connect4.restart();
+    })
 });
